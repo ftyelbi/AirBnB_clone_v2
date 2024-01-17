37e95d079eec391585171bc056ed877d7b12c3d0
ftyelbi <ftyelbi@gmail.com>
2024-01-17 15:11:58 -0800

    updated

--- models/city.py
+++ models/city.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-"""
-Module for the City class.
-"""
-from sqlalchemy import Column, String, ForeignKey
-from models.base_model import BaseModel, Base
+""" City Module for HBNB project """
+from models.base_model import BaseModel
 
-class City(BaseModel, Base):
-    """
-    City class for storing city information.
-    """
-    __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+
+class City(BaseModel):
+    """ The city class, contains state ID and name """
+    state_id = ""
+    name = ""       
--- models/state.py
+++ models/state.py
@@ -1,25 +1,8 @@
 #!/usr/bin/python3
-"""
-Module for the State class.
-"""
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
+""" State Module for HBNB project """
+from models.base_model import BaseModel
 
-class State(BaseModel, Base):
-    """
-    State class for storing state information.
-    """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
 
-    if storage_type == "db":
-        cities = relationship("City", backref="state", cascade="all, delete-orphan")
-    else:
-        @property
-        def cities(self):
-            """
-            Getter attribute that returns the list of City instances with state_id
-            equals to the current State.id.
-            """
-            return [city for city in storage.all("City").values() if city.state_id == self.id]
+class State(BaseModel):
+    """ State class """
+    name = "" 
