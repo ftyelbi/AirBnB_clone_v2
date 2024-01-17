7995ee444e5294fed9ffc9b9fdac1aa92c48e22d
Zakry27 <zak90078@gmail.com>
2024-01-17 07:20:28 -0800

    AirBnB_clone_v2 update

--- models/city.py
+++ models/city.py
@@ -1,30 +1,21 @@
 #!/usr/bin/python3
-""" holds class City"""
-import models
+"""city class"""
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
 from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+from models.place import Place
 
 
 class City(BaseModel, Base):
-    """the Representation of city """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'cities'
-        name = Column(String(128),
-                      nullable=False)
-        state_id = Column(String(60),
-                          ForeignKey('states.id'),
-                          nullable=False)
-        places = relationship("Place",
-                              backref="cities",
-                              cascade="all, delete-orphan")
-    else:
-        name = ""
-        state_id = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes city"""
-        super().__init__(*args, **kwargs)
+    """class for City
+    Attributes:
+        state_id: The state id
+        name: input name
+    """
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="cities")
