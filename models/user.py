510ca8af87ee786d54cad6fc2b9f265c43a4d781
ftyelbi <ftyelbi@gmail.com>
2024-01-17 14:41:46 -0800

    task 7 updated

--- models/user.py
+++ models/user.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
-"""This module defines User class"""
+"""
+Module for the User class.
+"""
 from sqlalchemy import Column, String
 from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
-
 
 class User(BaseModel, Base):
-    """ This class representing user by various attributes"""
-
-    __tablename__ = "users"
-
+    """
+    User class for storing user information.
+    """
+    __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-
-    # Relationships
-    places = relationship("Place", backref="user", cascade="delete")
-    reviews = relationship("Review", backref="user", cascade="delete")
