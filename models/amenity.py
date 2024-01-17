19607127740c575c6559fdca432e342a2af927bd
Zakry27 <zak90078@gmail.com>
2024-01-17 10:13:16 -0800

    AirBnB_clone_v2 update

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import String, Column
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    ''' class for Amenity '''
+    __tablename__ = 'amenities'
+
+    name = Column(String(128), nullable=False)
